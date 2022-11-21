d9698914e0eb5782e35a061b1013ad85407e0380
vadniks <hidden@some.a>
2022-11-21 16:27:19 +0300

    Still programming the logic

--- src/main/kotlin/com/example/app.kt
+++ src/main/kotlin/com/example/app.kt
@@ -9,13 +9,25 @@ import com.example.plugins.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import java.util.logging.Logger
 
-@Suppress("unused") val Any.unitStub get() = Unit
+val Any.unitStub get() = Unit
 
 fun main() = embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
     configureSecurity()
-    configureHTTP()
     configureRouting()
     install(ContentNegotiation) { json() }
     DatabaseFactory
+
+    val logger = Logger.getLogger("App")
+    logger.info("""
+        
+              :::::::: :::    ::::::::::::  ::::::::  :::::::: :::     ::: :::::::: 
+            :+:    :+::+:    :+::+:    :+::+:    :+::+:    :+::+:     :+::+:    :+: 
+           +:+       +:+    +:++:+    +:++:+       +:+    +:++:+     +:+      +:+   
+          +#+       +#+    +:++#++:++#: +#++:++#+++#+    +:++#+     +:+    +#+      
+         +#+       +#+    +#++#+    +#+       +#++#+    +#+ +#+   +#+   +#+         
+        #+#    #+##+#    #+##+#    #+##+#    #+##+#    #+#  #+#+#+#   #+#           
+        ########  ######## ###    ### ########  ########     ###    ##########      
+    """.trimIndent())
 }.start(wait = true).unitStub
